03f7285f0ac64be512e14c3212f79ec8a43f2647
indev <indev@i2pmail.org>
2021-03-26 12:27:28 +0300

    Better ore generation

--- mods/std/ores.py
+++ mods/std/ores.py
@@ -1,8 +1,13 @@
 from mapgen.ore import OreGen
 
+from config import getcfg
+
+
+config = getcfg()
+
 
 class CopperOreGen(OreGen):
     id = 'std:copper_ore'
     block = 'std:copper_ore'
-    min_y = 100
-    max_y = 500
+    min_y = int(config['mapgen.world_size'][1]*0.1)
+    max_y = int(config['mapgen.world_size'][1]*0.8)
