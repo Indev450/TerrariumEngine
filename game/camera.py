44ffd229480be683f74d7b1af094a127ffbafc76
indev <indev@i2pmail.org>
2021-04-05 14:46:36 +0300

    Smooth camera

--- game/camera.py
+++ game/camera.py
@@ -12,6 +12,8 @@ class Camera:
     def __init__(self, offset_x=0, offset_y=0):
         self.x = 0
         self.y = 0
+        self.last_x = 0
+        self.last_y = 0
         self.offset_x = offset_x
         self.offset_y = offset_y
         self.obj = None
@@ -20,11 +22,15 @@ class Camera:
     def update_position(self):
         if self.obj is None:
             return
-        self.x = self.obj.rect.x
-        self.y = self.obj.rect.y
+        self.last_x = (self.x + self.last_x) // 2
+        self.last_y = (self.y + self.last_y) // 2
+        self.x = self.obj.rect.centerx
+        self.y = self.obj.rect.centery
 
     def set_obj(self, obj):
         self.obj = obj
+        self.last_x = self.obj.rect.centerx
+        self.last_y = self.obj.rect.centery
         self.update_position()
 
     def move_offset(self, x, y):
@@ -36,4 +42,4 @@ class Camera:
         self.offset_y = y
 
     def get_position(self):
-        return self.x - self.offset_x, self.y - self.offset_y
+        return self.last_x - self.offset_x, self.last_y - self.offset_y
