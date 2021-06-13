af434e254c3583ba2f0aa3a00ec5c2737cc4edb6
indev <indev@i2pmail.org>
2021-06-13 18:29:17 +0300

    Add limit to sounds played at time

--- game/sound.py
+++ game/sound.py
@@ -1,4 +1,5 @@
 import traceback
+import time
 
 import pygame as pg
 
@@ -6,10 +7,12 @@ import pygame as pg
 class Sound:
     instances = {}
     
-    def __init__(self, name, volume=1.0):
+    def __init__(self, name, volume=1.0, min_wait=0.1):
         self.name = name
         self.sound = None
         self.volume = volume
+        self.min_wait = min_wait
+        self.last_played = 0
     
     def load(self, force=False):
         try:
@@ -23,18 +26,23 @@ class Sound:
             self.sound = pg.mixer.Sound(b'')
 
     def play(self, *args, **kwargs):
+        if time.time() - self.last_played < self.min_wait:
+            return
+        
         if self.sound is not None:
+            self.last_played = time.time()
+            
             self.sound.play(*args, **kwargs)
         else:
             traceback.print_stack()
             print(f'Error: sound {self.name} is not loaded')
 
 
-def getsound(name, volume=1.0):
+def getsound(name, volume=1.0, min_wait=0.1):
     if Sound.instances.get(name):
         return Sound.instances[name]
     
-    Sound.instances[name] = Sound(name, volume)
+    Sound.instances[name] = Sound(name, volume, min_wait)
     
     return Sound.instances[name]
 
