b10d4a1b48ea7a5d3f5b9e4c174d44db2aa44f42
indev <indev@i2pmail.org>
2021-06-21 17:36:53 +0300

    Deleted default config template from config.py

--- config.py
+++ config.py
@@ -1,46 +1,4 @@
-"""Module for loading and storing configuration file
-
-Default config:
-{
-    "app.resolution": [800, 640],
-    "app.font": "resources/fonts/dpcomic.ttf",
-    "app.font.size": 30,
-    "app.maxfps": 60,
-    "app.caption": "Terrarium",
-    
-    "menu.music": "resources/music/mainmenu.ogg",
-    "menu.background": "#5555FF",
-    
-    "game.music": "resources/music/game.ogg",
-    "game.background": "#5555FF",
-    
-    "mapgen.selected": "mapgenv1",
-    "mapgen.world_size": [1000, 500],
-    
-    "chunk.keep_alive_time": 5,
-    
-    "entity.default.gravity": 25,
-    "entity.default.braking": 0.7,
-    "entity.default.min_speed": 0.2,
-    "entity.default.max_fall": -20,
-    
-    "item.default.maxcount": 999,
-    "item.default.pick_up_sound": "resources/sounds/items/item_pick_up.wav",
-    
-    "item.entity.try_merge_freq": 1.0,
-    "item.entity.merge_dist": 1.5,
-    "item.entity.magnet_dist": 2,
-    "item.entity.magnet_acceleration": 50,
-    
-    "player.speed": 30,
-    "player.jump": 10,
-    "player.size": [20, 40],
-    
-    "tick.time": 1.0,
-    
-    "world.chunk_size": [10, 10]
-}
-"""
+"""Module for loading and storing configuration file"""
 import json
 
 
