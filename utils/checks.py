eacf41af7e5cee9bdf15f5b091e5cd2d9ec6e3a4
indev <indev@i2pmail.org>
2021-07-01 12:57:06 +0300

    Add hasitems() function

--- utils/checks.py
+++ utils/checks.py
@@ -15,3 +15,22 @@ def hasattrs(obj, attrs, listmissing=False):
         return len(missing) == 0, missing
     
     return True, ''
+
+
+def hasitems(collection, items, listmissing=False):
+    if not items:
+        return True, [] if listmissing else None
+    
+    missing = []
+    
+    for item in items:
+        if item not in collection:
+            if not listmissing:
+                return False, item
+            else:
+                missing.append(item)
+    
+    if listmissing:
+        return len(missing) == 0, missing
+    
+    return True, None
